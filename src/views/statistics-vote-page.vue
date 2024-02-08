22e80085d3eff260ff1e931709fc3327284262b8
oleg-yesterdays <olegyesterdays@gmail.com>
2024-02-08 20:12:22 +0300

    statistics page

--- src/views/statistics-vote-page.vue
+++ src/views/statistics-vote-page.vue
@@ -2,39 +2,40 @@
   <div class="background">
     <div class="statistics-panel">
       <div class="menu">
-        <button class="item button back" @click="$router.push('/adminPage')">Назад</button>
-        <div class="item itemNumberOfQuestions">
+        <button class="menu-item button" @click="$router.push('/adminPage')">Назад</button>
+
+        <div class="menu-item item-number-of-questions">
           <span>Количество вопросов</span>
           <span>{{ numberOfQuestions }}</span>
         </div>
-        <div class="item itemCreationDate">
+
+        <div class="menu-item item-creation-date">
           <span>Дата создания</span>
           <span>{{ creationDate }}</span>
         </div>
-        <div class="item itemUpdateDate">
-          <span>Дата обновления</span>
-          <span>{{ updateDate }}</span>
-        </div>
-        <div class="item itemNumberOfParticipants">
+
+        <div class="menu-item item-number-of-participants">
           <span>Количество участников</span>
           <span>{{ numberOfParticipants }}</span>
         </div>
-        <button class="item button">Завершить голосование</button>
-        <button class="item button">Редактировать голосование</button>
-        <button class="item button">Удалить голосование</button>
+
+        <button class="menu-item button">Завершить голосование</button>
+        <button class="menu-item button">Редактировать голосование</button>
+        <button class="menu-item button end">Удалить голосование</button>
       </div>
+
       <div class="statistics">
         <div class="item votingTitle">
           <img class="votingTitle-img" src="" alt="Картинка голосования"/>
           <div class="votingTitle-title">{{ votingTitle }}</div>
           <div class="votingTitle-description">{{ description }}</div>
         </div>
-        <div class="vote">
+        <div class="questions">
           <div class="item" v-for="({ title, answersToQuestions }, index) in questions" :key="index">
-            <div class="question">{{ title }}</div>
-            <div class="answer">
-              <div class="schedule" v-for="({ title, voteCount }, index) in answersToQuestions" :key="index">
-                <div>{{ title }}</div>
+            <span class="item-title">{{ title }}</span>
+            <div class="answers">
+              <div class="answer" v-for="({ title, voteCount }, index) in answersToQuestions" :key="index">
+                <span>{{ title }}</span>
                 <progress class="progress" max="100" :value="voteCount"/>
               </div>
             </div>
@@ -46,14 +47,13 @@
 </template>
 
 <script setup>
-import {computed} from "vue";
-import {useStore} from "vuex";
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 const store = useStore();
 
 const numberOfQuestions = computed(() => store.getters["statisticsModule/getNumberOfQuestions"]);
 const creationDate = computed(() => store.getters["statisticsModule/getCreationDate"]);
-const updateDate = computed(() => store.getters["statisticsModule/getUpdateDate"]);
 const numberOfParticipants = computed(() => store.getters["statisticsModule/getNumberOfParticipants"]);
 
 const questions = computed(() => store.getters["statisticsModule/getQuestions"]);
@@ -61,72 +61,79 @@ const votingTitle = computed(() => store.getters["statisticsModule/getVotingTitl
 const description = computed(() => store.getters["statisticsModule/getDescription"]);
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .background {
   display: flex;
 
   .statistics-panel {
     margin: 40px auto;
     display: flex;
-    height: 800px;
+    padding: 12px;
+    border-radius: 12px;
+    background: var(--secondary-light-theme);
+
+    .button {
+      background: var(--secondary-light-theme);
+      border: 0;
+      border-radius: 8px;
+      padding: 8px;
+      justify-content: center !important;
+
+      &:hover {
+        background: var(--accent-light-theme);
+        color: var(--white-light-theme);
+      }
+
+      &:active {
+        transform: scale(0.99);
+      }
+    }
+
+    .end {
+      margin: 0 !important;
+    }
 
     .menu {
+      padding: 8px;
+      border-radius: 8px;
       margin-right: 12px;
-      width: 300px;
-      display: flex;
-      flex-direction: column;
-      padding: 12px;
-      border-radius: 12px;
-      background: var(--secondary-light-theme);
-      height: 335px;
+      background: var(--white-light-theme);
 
-      .back {
-        margin-top: 0 !important;
-      }
-
-      .itemNumberOfQuestions, .itemCreationDate, .itemUpdateDate, .itemNumberOfParticipants {
+      &-item {
+        margin-bottom: 8px;
+        width: 100%;
         display: flex;
         justify-content: space-between;
-        border-bottom: 4px solid var(--white-light-theme);
-      }
-
-      .itemNumberOfParticipants {
-        border: 0;
-      }
-
-      .item {
-        margin-top: 8px;
-        padding: 8px;
-      }
-
-      .button {
-        background: var(--white-light-theme);
-        border: 0;
-        border-radius: 8px;
-
-        &:hover {
-          background: var(--accent-light-theme);
-          color: var(--white-light-theme);
-        }
-
-        &:active {
-          transform: scale(0.99);
-        }
       }
     }
 
     .statistics {
-      padding: 12px;
-      border-radius: 12px;
-      background: var(--secondary-light-theme);
+      width: 800px;
+      padding: 8px;
+      border-radius: 8px;
+      background: var(--white-light-theme);
 
-      .vote {
+      .votingTitle {
+        background: var(--secondary-light-theme);
+        padding: 8px;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+
+        &-img, &-title, &-description {
+          margin: 4px auto;
+        }
+      }
+
+      .questions {
+        border: 4px solid var(--secondary-light-theme);
         margin-top: 8px;
-        height: 670px;
-        width: 700px;
+        padding-right: 4px;
+        border-radius: 8px;
+        background: var(--secondary-light-theme);
+        height: 600px ;
         overflow: hidden;
         overflow-y: auto;
-        padding: 4px;
 
         &::-webkit-scrollbar {
           width: 8px;
@@ -142,70 +149,28 @@ const description = computed(() => store.getters["statisticsModule/getDescriptio
           border-radius: 4px;
           background: var(--white-light-theme);
         }
-      }
 
-      .votingTitle {
-        margin: 0 !important;
-        display: flex;
-        flex-direction: column;
-        //margin-bottom: 8px;
-
-        &-img {
-
-        }
-
-        &-title {
-          margin: 8px auto;
-        }
-      }
-
-      .item {
-        background: var(--white-light-theme);
-        border-radius: 8px;
-        padding: 8px;
-        margin-bottom: 20px;
-
-        &:last-child {
-          margin: 0;
-        }
-
-        .question {
-          background: var(--secondary-light-theme);
+        .item {
+          margin-bottom: 8px;
           padding: 8px;
           border-radius: 8px;
-        }
+          background: var(--white-light-theme);
 
-        .answer {
-          background: var(--secondary-light-theme);
-          border-radius: 8px;
+          &:last-child {
+            margin: 0;
+          }
 
-          .schedule {
-            margin-top: 8px;
+          .answers {
             padding: 8px;
+            border-radius: 8px;
+            background: var(--secondary-light-theme);
+            margin-top: 8px;
+            .answer {
+              display: flex;
+              flex-direction: column;
 
-            .progress {
-              width: 100%;
-              -webkit-appearance: none;
-              appearance: none;
-              border: none;
-              border-radius: 10px;
-              height: 10px;
-
-              &::-webkit-progress-bar {
-                background-color: var(--white-light-theme);
-              }
-
-              &::-webkit-progress-bar,
-              &::-webkit-progress-value {
-                border-radius: 10px;
-              }
-
-              &::-moz-progress-bar {
-                border-radius: 10px;
-              }
-
-              &::-webkit-progress-value {
-                background: var(--additional-light-theme);
+              .progress {
+                width: 100%;
               }
             }
           }
